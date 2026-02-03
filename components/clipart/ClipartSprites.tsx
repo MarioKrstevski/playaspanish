1b8f796a966a853cace3ebf3a156598ba27c9d5a
Mario Krstevski <mariokrstevski@hotmail.com>
2026-02-03 01:37:33 +0100

    animations done good

--- components/clipart/ClipartSprites.tsx
+++ components/clipart/ClipartSprites.tsx
@@ -24,20 +24,20 @@ export function ClipartSprites({ className }: ClipartSpritesProps) {
         className="clipart-hat absolute w-24 h-24 md:w-32 md:h-32 bg-contain bg-no-repeat bg-center"
         style={{
           backgroundImage: 'url(/mexican-sombrero.png)',
-          right: '20%',
+          right: '10%',
           top: '100px'
         }}
         aria-hidden
       />
-      {/* Third: slides in from right (staggered), then pulse
+      Third: slides in from right (staggered), then pulse
       <div
-        className="clipart-extra absolute left-[calc(50%-2rem)] md:left-[calc(50%-2.5rem)] w-16 h-16 md:w-20 md:h-20 bg-contain bg-no-repeat bg-center"
+        className="clipart-hat absolute left-[calc(50%-2rem)] md:left-[calc(50%-2.5rem)] w-16 h-16 md:w-20 md:h-20 bg-contain bg-no-repeat bg-center"
         style={{
-          backgroundImage: 'url(/mexico-flag.png)',
+          backgroundImage: 'url(/mexico-sombrero.png)',
           top: '80px'
         }}
         aria-hidden
-      /> */}
+      />
     </div>
   )
 }
